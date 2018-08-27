a978132145347f8e7c56872bfdc418a79189e526
Chudo <serhiichudnivets@gmail.com>
2018-08-27 23:22:22 +0300

    'rem_console'

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ function getNews() {
                  '<div class="card"> <div class="card-header"id="headingOne_' + i + '"><h5 class="mb-0"> <button class="btn btn-link"type="button" data-toggle="collapse" data-target="#collapseOne_' + i + '" aria-expanded="true" aria-controls="collapseOne">' + data.response.results[i].webTitle + '</button></h5></div><div id="collapseOne_' + i + '" class="collapse " aria-labelledby="headingOne_' + i + '" data-parent="#accordionExample"><div class="card-body"> <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.</p> <a href="' + data.response.results[i].webUrl + '" target="_blank">Read more</a> </div> </div> </div>';
             }
             list += '</div>';
-            console.log(data.response.results);
             all_pages.innerHTML = data.response.pages;
             app.innerHTML = list;
 
